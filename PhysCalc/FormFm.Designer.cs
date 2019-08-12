743c01009f322dc4916d5c256a4907bcea5b8f52
Chkup-dev <chkup@mail.ru>
2019-08-12 23:45:53 +0300

    Added Ft

---
+++ PhysCalc/FormFm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace PhysCalc
+{
+    partial class FormFm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.textBox7.Location = new System.Drawing.Point(119, 12);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(453, 56);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.Text = "Значение(H)";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.textBox6.Location = new System.Drawing.Point(119, 74);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(453, 56);
+            this.textBox6.TabIndex = 14;
+            this.textBox6.Text = "Значение(H/Кг)";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.textBox5.Location = new System.Drawing.Point(118, 136);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(453, 56);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "Результат(кг)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.textBox4.Location = new System.Drawing.Point(13, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 56);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "F= ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.textBox3.Location = new System.Drawing.Point(13, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 56);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "g= ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.textBox2.Location = new System.Drawing.Point(13, 136);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 56);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "m= ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.button1.Location = new System.Drawing.Point(408, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 67);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Считать";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
+            this.textBox1.Location = new System.Drawing.Point(12, 280);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(158, 68);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "m=F/g";
+            // 
+            // FormFm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FormFm";
+            this.Text = "FormFm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ PhysCalc/FormFm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhysCalc
+{
+    public partial class FormFm : Form
+    {
+        public FormFm()
+        {
+            InitializeComponent();
+        }
+    }
+}
